c2b8bd993afe4e7cec84f0e34c66e76bdfca9d82
Rohith S <rohi.devs@gmail.com>
2024-12-21 22:59:10 +0530

    ini

---
+++ src/test/java/ctf/projects/tech/backend/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ctf.projects.tech.backend.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
